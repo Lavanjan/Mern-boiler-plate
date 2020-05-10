9e3ea24f748495b33a31411731bf7bb952490fdd
lavanjan <ravilavanjan23@gmail.com>
2020-05-10 09:44:27 +0530

    Push on github

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+
+const config = require('./config/key');
+
+const { User } = require('./models/user');
+const { auth } = require("./middleware/auth");
+
+
+
+mongoose.connect(config.mongoURI,
+    { useNewUrlParser: true }).then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/api/user/auth",auth, (req, res) =>{
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+})
+})
+
+
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true,
+            userData: doc
+        })
+    })
+})
+app.post('/api/user/login', (req, res) => {
+    // find the email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Authentication failed, email not found"
+            });
+        // compare password
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({ loginsuccess: false, message: "wrong password" })
+            }
+        })
+        // generate password
+        user.generateToken((err, user) => {
+            if (err) return res.status(400).send(err);
+            res.cookie("x_auth", user.token)
+                .status(200)
+                .json({
+                    loginsuccess: true
+                })
+        })
+
+    })
+})
+
+app.listen(5000);
